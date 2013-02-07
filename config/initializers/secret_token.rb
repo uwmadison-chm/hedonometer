f798a6e30adecc1b3591a8cd18d1c8adae1bc589
Nate Vack <njvack@wisc.edu>
2013-02-07 13:35:03 -0600

    import all the rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hedonometer::Application.config.secret_token = 'fd29b9983fa4a92971998c1a435f21cd1d012cc43e0ce38888db23c37e523447bcd0d6d1112c07fc92c2abda87bcef2404409ece84af03587e65bd56c4b3871e'
