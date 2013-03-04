aa613a478269dacd05588ffb81af4e4e248734db
Nate Vack <njvack@wisc.edu>
2013-03-04 12:08:29 -0600

    Name test better

--- test/functional/admin/sessions_controller_test.rb
+++ test/functional/admin/sessions_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class Admin::SessionsControllerTest < ActionController::TestCase
-  test "should get new" do
+  test "should get new without login" do
     get :new
     assert_response :success
   end
