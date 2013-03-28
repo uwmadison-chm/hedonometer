391e36e6992c15c0abc396a14eef7b60db915d75
Nate Vack <njvack@wisc.edu>
2013-03-28 15:30:55 -0500

    Admins can only edit their surveys

--- app/controllers/admin/surveys_controller.rb
+++ app/controllers/admin/surveys_controller.rb
@@ -1,7 +1,16 @@
-class Admin::SurveysController < ApplicationController
+class Admin::SurveysController < AdminController
+  before_filter :find_survey, only: [:edit, :create]
+
   def new
+    @survey = Survey.new
   end
 
   def edit
+
+  end
+
+  private
+  def find_survey
+    @survey = current_admin.surveys.modifiable.where(id:params[:id]).first
   end
 end
--- test/functional/admin/surveys_controller_test.rb
+++ test/functional/admin/surveys_controller_test.rb
@@ -5,13 +5,32 @@ class Admin::SurveysControllerTest < ActionController::TestCase
     admin_login_as :nate
   end
 
+  test "new requires login" do
+    admin_logout
+    get :new
+    assert_response :redirect
+  end
+
   test "should get new" do
     get :new
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should get edit for editable survey" do
     get :edit, id:surveys(:test).id
     assert_response :success
+    assert assigns(:survey)
+  end
+
+  test "should not make form for orphaned survey" do
+    get :edit, id:surveys(:orphaned).id
+    assert_response :success
+    assert_nil assigns(:survey)
+  end
+
+  test "should not make form for noneditable survey" do
+    get :edit, id:surveys(:someone_elses).id
+    assert_response :success
+    assert_nil assigns(:survey)
   end
 end
