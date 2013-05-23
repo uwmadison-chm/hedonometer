fed1081eeecc0bc47087e60dbb6bd9a43f1be743
Nate Vack <njvack@wisc.edu>
2013-05-23 13:27:11 -0500

    Update test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,14 +5,12 @@ Hedonometer::Application.configure do
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs. Don't rely on the data there!
+  config.eager_load = false
   config.cache_classes = true
 
   # Configure static asset server for tests with Cache-Control for performance
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=3600"
-
-  # Log error messages when you accidentally call methods on nil
-  config.whiny_nils = true
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
