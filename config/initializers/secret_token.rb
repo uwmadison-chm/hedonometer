7f4517f9c1607b26eca88fec7ac8e9e98a7311be
Nate Vack <njvack@wisc.edu>
2013-05-23 13:51:57 -0500

    Sure, we'll encrypt our cookie session

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,5 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Hedonometer::Application.config.secret_token = 'fd29b9983fa4a92971998c1a435f21cd1d012cc43e0ce38888db23c37e523447bcd0d6d1112c07fc92c2abda87bcef2404409ece84af03587e65bd56c4b3871e'
+
+Hedonometer::Application.config.secret_key_base = '48f2a80781d6f7f5d2876fb112be8f7cc664945fa52bf633d3a5e420807b2b455481508a99518f1fff465703debf784729e65afb8efc88313984003e0eef0f77'
