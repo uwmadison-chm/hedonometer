7506e1c16c195f994bb0b74da449c057a7674e29
Nate Vack <njvack@wisc.edu>
2013-06-05 16:26:21 -0500

    Add validations and creators to surveys

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,4 +1,21 @@
 class Survey < ActiveRecord::Base
+  attr_accessor :creator
+
   has_many :survey_permissions
-  has_many :admins, :through => :survey_permissions
+  has_many :admins, through: :survey_permissions
+
+  validates :creator, presence: true, on: :create
+  validates :name, presence: true
+  validates :samples_per_day, numericality: {only_integer: true, greater_than: 0}
+  validates :mean_minutes_between_samples,
+    numericality: {only_integer: true, greater_than: 0}
+  validates :sample_minutes_plusminus,
+    numericality: {only_integer: true, greater_than: 0}
+
+  after_create :assign_creator_as_admin
+
+  private
+  def assign_creator_as_admin
+    self.survey_permissions.create admin: creator, can_modify_survey: true
+  end
 end
--- test/models/survey_test.rb
+++ test/models/survey_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class SurveyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  end
+
+  def sample_survey_params
+    {
+      creator: admins(:nate),
+      name: "test1",
+      samples_per_day: 1,
+      mean_minutes_between_samples: 15,
+      sample_minutes_plusminus: 5,
+      active: false
+    }
+  end
+
+  test "create a simple survey" do
+    s = Survey.create sample_survey_params
+    assert s.valid?
+    refute s.new_record?
+  end
+
+  test "validations are firing" do
+    p = sample_survey_params
+    p.delete(:creator)
+    s = Survey.create p
+    refute s.valid?
+  end
+
+  test "creator gets assigned as an admin" do
+    s = Survey.create sample_survey_params
+    assert_equal s.admins.first, admins(:nate)
+    assert admins(:nate).can_modify_survey?(s)
+  end
 end
