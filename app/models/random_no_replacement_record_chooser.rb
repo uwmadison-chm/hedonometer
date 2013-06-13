778daa69e8308f77b714af95108a1c8cbd3e1ff8
Nate Vack <njvack@wisc.edu>
2013-06-13 14:17:09 -0500

    A random ActiveRecord chooser
    This one chooses randomly, without replacement.

---
+++ app/models/random_no_replacement_record_chooser.rb
@@ -0,0 +1,22 @@
+class RandomNoReplacementRecordChooser
+  attr_reader :record_set, :unused_id_list
+  def initialize(record_set, unused_id_array=[])
+    @record_set = record_set
+    @unused_id_list = unused_id_array
+    allow_all_ids_if_empty!
+  end
+
+  def choose
+    id_to_choose = @unused_id_list.sample
+    @unused_id_list.delete id_to_choose
+    allow_all_ids_if_empty!
+    @record_set.find id_to_choose
+  end
+
+  private
+  def allow_all_ids_if_empty!
+    if @unused_id_list.empty?
+      @unused_id_list = @record_set.pluck :id
+    end
+  end
+end
---
+++ test/models/random_no_replacement_record_chooser_test.rb
@@ -0,0 +1,41 @@
+require 'test_helper'
+
+class RandomNoReplacementRecordChooserTest < ActiveSupport::TestCase
+  def setup
+    @survey = surveys(:test)
+    @all_questions = @survey.survey_questions
+    @times_to_try_random_things = 10 # Yes this is janky
+  end
+
+  test "it should set all ids when empty" do
+    all_ids = @all_questions.pluck :id
+    chooser = RandomNoReplacementRecordChooser.new(@all_questions, [])
+    assert_equal all_ids, chooser.unused_id_list
+  end
+
+
+  test "it should draw a question" do
+    chooser = RandomNoReplacementRecordChooser.new(@all_questions)
+    q = chooser.choose
+    assert_includes @all_questions, q
+  end
+
+  test "it should draw all questions" do
+    @times_to_try_random_things.times do
+      chooser = RandomNoReplacementRecordChooser.new(@all_questions)
+      chosen = (0...@all_questions.count).map {|i|
+        chooser.choose
+      }.uniq
+      assert_equal chosen.length, @all_questions.count
+    end
+  end
+
+  test "it will draw more questions" do
+    chooser = RandomNoReplacementRecordChooser.new(@all_questions)
+    chosen = (0...(@all_questions.count+1)).map {|i|
+      chooser.choose
+    }
+    assert_equal (@all_questions.count+1), chosen.size
+  end
+
+end
