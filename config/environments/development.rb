a851b16fff45072977a64006bb74186589b8b362
Nate Vack <njvack@wisc.edu>
2013-07-11 16:35:18 -0500

    Fix line numbers in css

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,7 @@ Hedonometer::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-  config.sass.debug_info = true
-  config.sass.line_comments = false # source maps don't get output if this is true
+  #config.sass.debug_info = true
+  config.sass.line_numbers = true
+  config.sass.trace_selectors = true
 end
