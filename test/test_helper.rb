3e6020b2f6f5373e9426f0e45ef32c0df44c85cb
Nate Vack <njvack@wisc.edu>
2013-07-22 17:17:35 -0500

    Add a way to mock twilio calls
    because I don't want my test harness sending
    test messages

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,6 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-
   fixtures :all
 
   def admin_login_as(fixture_name)
@@ -16,5 +15,31 @@ class ActiveSupport::TestCase
 
   def admin_id_set?
     session.include? :admin_id
+  end
+
+  def mock_twilio_service(stub_client)
+    Twilio::REST::Client.stub :new, stub_client do
+      yield
+    end
+  end
+
+end
+
+class Recordy
+  attr_accessor :calls
+  def initialize
+    @calls = []
+  end
+
+  def method_missing(method, *args)
+    ret = self.class.new
+    @calls << {
+      method: method,
+      args: args,
+      block_given: block_given?,
+      ret: ret
+    }
+    yield if block_given?
+    ret
   end
 end
