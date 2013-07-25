54b98483cde8b9cdb7f716574948bd4611bfdfba
Nate Vack <njvack@wisc.edu>
2013-07-25 16:28:33 -0500

    Make a nice way to record Recordy mock calls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -24,24 +24,47 @@ class ActiveSupport::TestCase
       yield
     end
   end
+end
 
+module MiniTest::Assertions
+  def assert_changes(obj, method, exp_diff)
+    before = obj.send method
+    yield
+    after  = obj.send method
+    diff = after - before
+
+    assert_equal exp_diff, diff, "Expected #{obj.class.name}##{method} to change by #{exp_diff}, changed by #{diff}"
+  end
+
+  def assert_method_chain(recordy, method_array)
+    # for example, assert_method_chain(client, [:account, :sms: :messages, :create])
+    assert method_chain_contains(recordy, method_array), "Expected method chain #{method_array.join('.')}"
+  end
+
+  def method_chain_contains(recordy, method_array)
+    cur_method, *rest = *method_array
+    return true if cur_method.nil?
+    call = recordy.__calls.detect {|call| call[:method].to_s == cur_method.to_s}
+    call && method_chain_contains(call[:return_value], rest)
+  end
 end
 
 class Recordy
-  attr_accessor :calls
+  attr_accessor :__calls
   def initialize
-    @calls = []
+    @__calls = []
   end
 
   def method_missing(method, *args)
-    ret = self.class.new
-    @calls << {
+    return_value = self.class.new
+    @__calls << {
       method: method,
       args: args,
       block_given: block_given?,
-      ret: ret
+      return_value: return_value
     }
     yield if block_given?
-    ret
+    return_value
   end
 end
+
