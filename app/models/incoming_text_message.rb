da8c27240455151189061b3e5088eea6c1e40628
Nate Vack <njvack@wisc.edu>
2013-07-29 17:20:14 -0500

    Validations and such for incoming messages

--- app/models/incoming_text_message.rb
+++ app/models/incoming_text_message.rb
@@ -1,2 +1,15 @@
 class IncomingTextMessage < TextMessage
+  before_validation :set_delivered_at, on: :create
+  validate :survey_phone_number_matches_to_phone_number
+
+  protected
+  def set_delivered_at
+    self.delivered_at = Time.now
+  end
+
+  def survey_phone_number_matches_to_phone_number
+    if survey.phone_number != self.to
+      errors.add(:to, "must match survey number (#{survey.phone_number})")
+    end
+  end
 end
--- test/models/incoming_text_message_test.rb
+++ test/models/incoming_text_message_test.rb
@@ -2,4 +2,21 @@ require 'test_helper'
 
 class IncomingTextMessageTest < ActiveSupport::TestCase
 
+  test "validation sets delivered_at" do
+    tm = surveys(:test).incoming_text_messages.build
+    tm.valid?
+    refute_nil tm.delivered_at
+  end
+
+  test "survey phone number must match To: number" do
+    tm = surveys(:test).incoming_text_messages.build(
+      to: surveys(:test).phone_number,
+      from: '+16085559999',
+      message: "test"
+    )
+    assert_valid tm
+    tm.to = PhoneNumber.new("+16665551212")
+    refute_valid tm
+  end
+
 end
