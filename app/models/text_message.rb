49103f84afb4b518dc7c21de665834aa3ecbfd37
Nate Vack <njvack@wisc.edu>
2013-07-29 17:18:37 -0500

    Validation and serialization for TextMessages

--- app/models/text_message.rb
+++ app/models/text_message.rb
@@ -1,5 +1,14 @@
 class TextMessage < ActiveRecord::Base
   belongs_to :survey
+  serialize :server_response, JSON
+
+  serialize :from, PhoneNumber
+  serialize :to, PhoneNumber
+
+  validates :from, presence: true
+  validates :to, presence: true
+
+  validates :survey, presence: true
 
   class DeliveryError < RuntimeError
   end
