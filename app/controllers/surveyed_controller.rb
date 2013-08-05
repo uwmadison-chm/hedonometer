1d59f1916d1f74d214e3b86bf40be6675b89d0e1
Nate Vack <njvack@wisc.edu>
2013-08-05 10:10:54 -0500

    Do something saner for find_survey

--- app/controllers/surveyed_controller.rb
+++ app/controllers/surveyed_controller.rb
@@ -4,11 +4,18 @@ class SurveyedController < ApplicationController
   before_action :find_survey
 
   protected
+  def current_survey
+    @survey ||= Survey.find params[:survey_id]
+  end
+
   def find_survey
-    @survey = Survey.find params[:survey_id]
+    current_survey
+  end
+
+  def current_participant
   end
 
   def require_participant_login!
-    # stuff
+
   end
 end
