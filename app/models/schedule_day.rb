8ec85cc8183a7f2aadb413d53026d7599da6c64d
Nate Vack <njvack@wisc.edu>
2013-08-14 16:32:24 -0500

    Now we can schedule things in the future

--- app/models/schedule_day.rb
+++ app/models/schedule_day.rb
@@ -10,7 +10,7 @@ class ScheduleDay < ActiveRecord::Base
   # TODO: Add validation for time_ranges_string
 
   def time_ranges_string
-    self.time_ranges.join(", ")
+    self.time_ranges.map {|tr| tr.to_s}.join(", ")
   end
 
   def time_ranges_string=(str)
@@ -19,5 +19,29 @@ class ScheduleDay < ActiveRecord::Base
       self.time_ranges = []
     end
     self.time_ranges = str.split(", ").map {|range_str| TimeRange.from_date_and_string(self.date, range_str)}
+  end
+
+  def valid_future_time_from(start_at, future_seconds)
+    ranges_to_search = self.truncated_time_ranges_starting_at(start_at)
+    ranges_to_search.each do |range|
+      if range.duration >= future_seconds
+        return range.first + future_seconds
+      end
+      future_seconds -= range.duration
+    end
+  end
+
+  def time_range_for(t)
+    # Finds the time range in which t happens, or the possible one. Or nil.
+    sorted = time_ranges.sort_by {|tr| tr.first}
+    sorted.detect {|tr| tr.include? t} || sorted.detect {|tr| tr.first > t}
+  end
+
+  def truncated_time_ranges_starting_at(t)
+    ranges = time_ranges.find_all {|tr| tr.end >= t}.map {|tr| tr.dup}
+    return ranges if ranges.empty?
+    new_start_time = [t, ranges[0].first].max
+    ranges[0] = TimeRange.new(new_start_time, ranges[0].end)
+    ranges
   end
 end
--- test/models/schedule_day_test.rb
+++ test/models/schedule_day_test.rb
@@ -1,4 +1,60 @@
 require 'test_helper'
 
 class ScheduleDayTest < ActiveSupport::TestCase
+
+  def setup
+    @sd = ScheduleDay.new
+    @sd.time_ranges = sample_time_ranges
+    @start = sample_time_ranges[0].first
+    super
+  end
+
+  def sample_time_ranges
+    t1 = Time.zone.parse("2013-05-04 10:00 AM")
+    @sample_time_ranges ||= [
+      TimeRange.new(t1, t1+1.hour),
+      TimeRange.new(t1+2.hours, t1+3.hours),
+      TimeRange.new(t1+(3.5).hours, t1+(3.75).hours)]
+  end
+
+  test "time bumping" do
+    t = @sd.valid_future_time_from(@start, 30.minutes)
+    assert_equal (@start + 30.minutes), t
+    t = @sd.valid_future_time_from(@start, 90.minutes)
+    assert_equal (@start + 150.minutes), t
+  end
+
+  test "find time range inside" do
+    tr = @sd.time_range_for(@start)
+    assert_equal @sample_time_ranges[0], tr
+    tr = @sd.time_range_for(@start + 150.minutes)
+    assert_equal @sample_time_ranges[1], tr
+  end
+
+  test "find time range with bumping" do
+    tr = @sd.time_range_for(@start + 61.minutes)
+    assert_equal @sample_time_ranges[1], tr
+    tr = @sd.time_range_for(@start + 4.hours)
+    assert_nil tr
+  end
+
+  test "truncate time ranges from start" do
+    test_start = @start + (2.5).hours
+    ranges = @sd.truncated_time_ranges_starting_at(test_start)
+    assert_equal 2, ranges.length
+    assert_equal test_start, ranges[0].first
+  end
+
+  test "truncate time ranges between ranges" do
+    test_start = @start + (1.5).hours
+    ranges = @sd.truncated_time_ranges_starting_at(test_start)
+    assert_equal 2, ranges.length
+    assert_equal (@start + 2.hours), ranges[0].first
+  end
+
+  test "truncate returns nothing if too late" do
+    test_start = @start + 4.hours
+    ranges = @sd.truncated_time_ranges_starting_at(test_start)
+    assert_empty ranges
+  end
 end
