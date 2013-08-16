01b977a1e5663ba49919336a5a4cf87751d351aa
Nate Vack <njvack@wisc.edu>
2013-08-16 11:24:32 -0500

    Add some smarts to ScheduledQuestion

--- app/models/scheduled_question.rb
+++ app/models/scheduled_question.rb
@@ -1,7 +1,28 @@
 class ScheduledQuestion < ActiveRecord::Base
   belongs_to :schedule_day
+  validates :schedule_day, presence: true
+
   belongs_to :survey_question
+  validates :survey_question, presence: true
+
+  validates :scheduled_at, presence: true
 
   scope :delivered, -> { where.not(delivered_at: nil).order('scheduled_at desc') }
   scope :undelivered, -> { where(delivered_at: nil).order('scheduled_at desc') }
+
+  def can_be_delivered_now?(max_age)
+    undelivered? and delivery_due? and younger_than?(max_age)
+  end
+
+  def undelivered?
+    delivered_at.nil?
+  end
+
+  def delivery_due?
+    scheduled_at and scheduled_at <= Time.now
+  end
+
+  def younger_than?(seconds)
+    scheduled_at and scheduled_at <= Time.now - seconds
+  end
 end
--- test/models/scheduled_question_test.rb
+++ test/models/scheduled_question_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class ScheduledQuestionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "undelivered when delivered_at is nil" do
+    sq = ScheduledQuestion.new
+    assert sq.undelivered?
+    sq.delivered_at = Time.now
+    refute sq.undelivered?
+  end
+
+  test "delivery is due when time is past scheduled_at" do
+    sq = ScheduledQuestion.new scheduled_at: (Time.now - 1.minute)
+    assert sq.delivery_due?
+    sq.scheduled_at = (Time.now + 1.minute)
+    refute sq.delivery_due?
+  end
+
+  test "older than x seconds" do
+    sq = ScheduledQuestion.new scheduled_at: (Time.now - 1.minute)
+    assert sq.younger_than?(30.seconds)
+    refute sq.younger_than?(2.minutes)
+  end
+
+  test "can be delivered now" do
+    sq = ScheduledQuestion.new scheduled_at: Time.now - 1.minute
+    assert sq.can_be_delivered_now?(30.seconds)
+    refute sq.can_be_delivered_now?(2.minutes)
+    sq.delivered_at = Time.now
+    refute sq.can_be_delivered_now?(30.seconds)
+    sq.delivered_at = nil
+    sq.scheduled_at = Time.now + 1.minute
+    refute sq.can_be_delivered_now?(30.seconds)
+  end
 end
