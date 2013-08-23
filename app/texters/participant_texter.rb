027ed36ca7fff610793b6b819a302ab3bf6d9363
Nate Vack <njvack@wisc.edu>
2013-08-23 14:13:07 -0500

    Tighten up login code message

--- app/texters/participant_texter.rb
+++ app/texters/participant_texter.rb
@@ -1,7 +1,7 @@
 class ParticipantTexter < ActionTexter::Base
   class << self
     def login_code_message(participant)
-      message_for_participant(participant, "Your login code is #{participant.login_code}. You may quit at any time by texting 'STOP'.")
+      message_for_participant(participant, "Your login code is #{participant.login_code}. Quit at any time by texting STOP.")
     end
 
     def deliver_scheduled_question!(scheduled_question_id)
