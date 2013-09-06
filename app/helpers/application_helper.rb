75375a46813c5cc7dfd8c7529f66d6f4396960fb
Nate Vack <njvack@wisc.edu>
2013-09-06 12:22:01 -0500

    Make the grid only show in development

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
   def humanize_phone_number(numberlike)
     PhoneNumber.new(numberlike.to_s).humanize
   end
+
+  def grid_if_development
+    'grid' if Rails.env.development?
+  end
 end
