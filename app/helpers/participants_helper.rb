e374e55bda2b1d5c3271b780d5fd0e82eb44f98d
Nate Vack <njvack@wisc.edu>
2014-05-01 11:22:56 -0500

    Add a warning about day length requirements to ppt#edit

--- app/helpers/participants_helper.rb
+++ app/helpers/participants_helper.rb
@@ -1,4 +1,13 @@
 # -*- encoding : utf-8 -*-
 
 module ParticipantsHelper
+    def day_length_string(total_minutes)
+        hours = (total_minutes/60).to_i
+        minutes = total_minutes%60
+        minutes_string = ''
+        if minutes > 0
+            minutes_string = ' '+pluralize(minutes, 'minute')
+        end
+        "#{pluralize(hours, 'hour')}#{minutes_string}"
+    end
 end
