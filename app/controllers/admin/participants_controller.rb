ecb59fa5d48a2d161b0480db8a5472a088eb4572
Nate Vack <njvack@wisc.edu>
2014-05-15 12:00:48 -0500

    Add participant details view.

--- app/controllers/admin/participants_controller.rb
+++ app/controllers/admin/participants_controller.rb
@@ -2,5 +2,13 @@ class Admin::ParticipantsController < AdminController
   def index
   end
 
+  def show
+  end
+
+  def current_participant
+    @participant ||= current_survey.participants.find(params[:id])
+  end
+  helper_method :current_participant
+
   protected
 end
