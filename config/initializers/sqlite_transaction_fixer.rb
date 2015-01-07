b47f5e9be157248f7bf42113f6cd58f725510c79
Nate Vack <njvack@wisc.edu>
2015-01-07 11:26:00 -0600

    Try this again for Ruby 1.9.x

--- config/initializers/sqlite_transaction_fixer.rb
+++ config/initializers/sqlite_transaction_fixer.rb
@@ -3,16 +3,12 @@
 # for a lock on the database file.
 # I know, use a real goddamn database.
 
-module SqliteTransactionFix
-  def begin_db_transaction
-    log('begin immediate transaction', nil) { @connection.transaction(:immediate) }
-  end
-end
-
 module ActiveRecord
   module ConnectionAdapters
     class SQLiteAdapter < AbstractAdapter
-      prepend SqliteTransactionFix
+      def begin_db_transaction
+        log('begin immediate transaction', nil) { @connection.transaction(:immediate) }
+      end
     end
   end
 end
