ade29c2b8e0b035071aa49d82513b157f8d9f276
Nate Vack <njvack@wisc.edu>
2015-01-14 10:29:33 -0600

    Change column type for password storage

--- db/migrate/20130305155723_change_admin_password_storage.rb
+++ db/migrate/20130305155723_change_admin_password_storage.rb
@@ -2,7 +2,7 @@
 class ChangeAdminPasswordStorage < ActiveRecord::Migration
   def change
     remove_column :admins, :password_digest
-    add_column :admins, :password_salt, :string
-    add_column :admins, :password_hash, :string
+    add_column :admins, :password_salt, :binary, :limit => 1.kilobyte
+    add_column :admins, :password_hash, :binary, :limit => 1.kilobyte
   end
 end
