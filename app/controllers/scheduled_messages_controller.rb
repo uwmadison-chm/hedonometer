7d2b88dceaa1a5d7b7dbe56bb952dc22310b6b3b
Dan Fitch <dgfitch@google.com>
2019-02-26 08:30:40 -0600

    Baby steps for link redirection

--- app/controllers/scheduled_messages_controller.rb
+++ app/controllers/scheduled_messages_controller.rb
@@ -1,22 +1,22 @@
 class ScheduledMessagesController < SurveyedController
   layout 'participant'
 
-  # Don't require login
+  # Don't require login, we just need to redirect people to a link
   skip_before_action :require_participant_login!
 
   def show
     message = ScheduledMessage.find_by_id params[:id]
     if message
-      # Set this for views
+      # Set this for views so they can show survey's help message
       @current_survey = message.schedule_day.participant.survey
 
       # How old are we? If too old, show expired message
-      if message.scheduled_at > 30.minutes then
+      if (Time.now - message.scheduled_at) > 30.minutes then
         @expired_string = "This link expired #{helpers.time_ago_in_words(message.scheduled_at + 30.minutes)} ago."
         render "expired"
       else
         @expired_string = nil
-        redirect_to @current_survey.something
+        redirect_to "http://current/something" #@current_survey.something
       end
     else
       render "expired", status: :not_found
--- test/controllers/scheduled_messages_controller_test.rb
+++ test/controllers/scheduled_messages_controller_test.rb
@@ -2,12 +2,18 @@ require 'test_helper'
 
 class ScheduledMessagesControllerTest < ActionController::TestCase
   test "current message should redirect" do
-    post :show, params: {id: scheduled_messages(:message_2).id}
+    m = scheduled_messages(:message_2)
+    m.scheduled_at = Time.now - 10.minutes
+    m.save!
+    post :show, params: {id: m.id}
     assert_redirected_to %r(\Ahttp://something)
     refute assigns(:expired_string)
   end
 
   test "older than 30 minutes message should show expired message" do
+    m = scheduled_messages(:message_1)
+    m.scheduled_at = Time.now - 40.minutes
+    m.save!
     post :show, params: {id: scheduled_messages(:message_1).id}
     assert_response :success
     assert_select 'h3', /expired \d+ minutes ago/
