487b71d1c3c58ad563b0046f53535abbc1773954
Dan Fitch <dgfitch@gmail.com>
2019-03-18 16:47:31 -0500

    Change order of operations for postgres production migration

--- db/migrate/20190222230454_rename_scheduled_question_to_scheduled_message.rb
+++ db/migrate/20190222230454_rename_scheduled_question_to_scheduled_message.rb
@@ -1,6 +1,6 @@
 class RenameScheduledQuestionToScheduledMessage < ActiveRecord::Migration[5.2]
   def change
+    rename_index :scheduled_questions, 'index_scheduled_questions_on_aasm_state', 'index_scheduled_messages_on_aasm_state'
     rename_table :scheduled_questions, :scheduled_messages
-    rename_index :scheduled_messages, 'index_scheduled_questions_on_aasm_state', 'index_scheduled_messages_on_aasm_state'
   end
 end
