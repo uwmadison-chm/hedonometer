79411de665c12fc528d23e3b504126709bd00b55
Dan Fitch <dgfitch@gmail.com>
2019-05-24 09:06:15 -0500

    Tweaks to participant display to view url that went with messages

--- app/helpers/admin/participants_helper.rb
+++ app/helpers/admin/participants_helper.rb
@@ -1,6 +1,6 @@
 module Admin::ParticipantsHelper
   def number_with_external_key(ppt)
     num = "#{ppt.phone_number.humanize}"
-    num += " (#{ppt.external_key})" if ppt.external_key.present?
+    num += " [#{ppt.external_key}]" if ppt.external_key.present?
   end
 end
